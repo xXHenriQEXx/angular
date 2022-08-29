1471381a16d75d54941d87decdbb891fe06ffdf6
Henrique Santos <henriqe.e2@gmail.com>
2022-08-29 00:04:26 -0300

    refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NovaTransferenciaComponent
+    NovaTransferenciaComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class NovaTransferenciaComponent {
 
+   valor: number = 23;
+   destino: number = 22;
+
+  transf(){
+    console.log("transferencia feita com sucesso");
+    console.log("valor", this.valor);
+    console.log("destino", this.destino);
+  }
+
+
+
 }
