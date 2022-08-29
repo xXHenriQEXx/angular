bea8769f3f735594472b5a11d510a49251d0d09b
Henrique Santos <henriqe.e2@gmail.com>
2022-08-28 23:14:59 -0300

    commit-projeto

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NovaTransferenciaComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
